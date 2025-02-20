7831411b0255ae24c6e1e763b2d558abf42ba3b9
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-20 19:59:12 -0300

    Criando o Cabeçalho

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@
 
 import { styled } from "styled-components"
 import EstilosGlobais from './componentes/EstilosGlobais/index';
+import Cabecalho from "./componentes/Cabecalho";
+import CampoTexto from "./componentes/CampoTexto";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
+  
+  img {
+    max-width: 212px;
+  }
   `
 
 function App() {
@@ -14,6 +20,7 @@ function App() {
   return (
     <FundoGradiente>
       <EstilosGlobais />
+      <Cabecalho />
     </FundoGradiente>
   )
 }
