1feaff6df80b9bb7c2c418f3d482de12c5af91cd
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-20 17:31:57 -0300

    Resetando Estilos Globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // Início Aula-1
 
 import { styled } from "styled-components"
+import EstilosGlobais from './componentes/EstilosGlobais/index';
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -12,7 +13,7 @@ function App() {
 
   return (
     <FundoGradiente>
-      
+      <EstilosGlobais />
     </FundoGradiente>
   )
 }
