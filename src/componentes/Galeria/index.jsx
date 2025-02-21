aef00b22879cef571d8c0ec0ba4f8c74e9922156
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-21 18:38:30 -0300

    Estruturando a galeria

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,7 +1,31 @@
+import { styled } from "styled-components";
+import Titulo from "../Titulo";
+import Tags from "./Tags";
+import Populares from './Populares';
+
+const GaleriaContainer = styled.div`
+  display: flex;
+`;
+
+const SecaoFluida = styled.section`
+  flex-grow: 1;
+`;
+
 const Galeria = () => {
-    return (
-      <h2>Navegue pela galeria</h2>
-    )
-  }
-  
-  export default Galeria;
+  return (
+    <>
+      <Tags />
+      <GaleriaContainer>
+
+        <SecaoFluida>
+          <Titulo>Navegue pela galeria</Titulo>
+        </SecaoFluida>
+
+        <Populares />
+
+      </GaleriaContainer>
+    </>
+  );
+};
+
+export default Galeria;
