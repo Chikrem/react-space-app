0dfc784bf0260aff827930073da07cd82f5164a6
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-21 18:13:56 -0300

    Montando Layout Desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
-import { styled } from "styled-components"
-import EstilosGlobais from "./componentes/EstilosGlobais"
-import Cabecalho from "./componentes/Cabecalho"
-import BarraLateral from "./componentes/BarraLateral"
-import Banner from "./componentes/Banner"
-import bannerBackground from './assets/banner.png'
+import { styled } from "styled-components";
+import EstilosGlobais from "./componentes/EstilosGlobais";
+import Cabecalho from "./componentes/Cabecalho";
+import BarraLateral from "./componentes/BarraLateral";
+import Banner from "./componentes/Banner";
+import Galeria from "./componentes/Galeria";
+import bannerBackground from "./assets/banner.png";
 
 const FundoGradiente = styled.div`
-  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
   width: 100%;
   min-height: 100vh;
-`
+`;
 
 const AppContainer = styled.div`
-width: 1440px;
-margin: 0 auto;
-max-width:100%;
-`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`;
 
 const MainContainer = styled.main`
-    display: flex;
-    gap: 24px;
-`
+  display: flex;
+  gap: 24px;
+`;
 
-function App() {
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
 
+function App () {
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -31,14 +42,17 @@ function App() {
         <Cabecalho />
         <MainContainer>
           <BarraLateral />
-          <Banner
-            texto="A galeria mais completa de fotos do espaço!"
-            backgroundImage={bannerBackground}
-          />
+                    <ConteudoGaleria>
+            <Banner
+              texto="A galeria mais completa de fotos do espaço!"
+              backgroundImage={bannerBackground}
+            />
+                        <Galeria />
+                    </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
     </FundoGradiente>
   )
 }
 
-export default App
+export default App;
