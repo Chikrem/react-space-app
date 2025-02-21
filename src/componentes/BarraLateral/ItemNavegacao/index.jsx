9e97890e80d7f8f4d3ca0df7a716b81aa3717769
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-21 17:09:53 -0300

    Ajustando Itens

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -1,13 +1,28 @@
-import { styled } from "styled-components"
+import { styled } from "styled-components";
 
 const ItemListaEstilizado = styled.li`
-`
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    color: ${ props => props.$ativo ? '#7B78E5' : '#D9D9D9'};
+    display: flex;
+    align-items: center;
+    gap: 22px;
+`;
 
-const ItemNavegacao = ({children, iconeAtivo, iconeInativo, ativo = false}) => {
-  return <ItemListaEstilizado>
+const ItemNavegacao = ({
+  children,
+  iconeAtivo,
+  iconeInativo,
+  ativo = false,
+}) => {
+  return (
+    <ItemListaEstilizado $ativo={ativo}>
       <img src={ativo ? iconeAtivo : iconeInativo} alt="" />
       {children}
-  </ItemListaEstilizado>
-}
+    </ItemListaEstilizado>
+  );
+};
 
-export default ItemNavegacao
+export default ItemNavegacao;
